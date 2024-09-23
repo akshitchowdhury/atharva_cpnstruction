dbafed4a9e84ef3c333139baae5084044b97ec38
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-09-23 11:32:47 +0530

    gallery2

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -17,8 +17,8 @@ const Gallery = () => {
     <GalleryBranded/>
     <AboutQuickConnect/>
     
-    <GalleryBranded/>
-    <AboutQuickConnect/>
+    {/* <GalleryBranded/>
+    <AboutQuickConnect/> */}
     </>
   )
 }
